70dc35aa0f2f2376d4b2c41b1e46cc63eff4eda3
Cory Benfield <lukasaoz@gmail.com>
2013-07-17 18:55:55 +0100

    Actions should take args and kwargs.

--- irc/action.go
+++ irc/action.go
@@ -5,9 +5,9 @@ import (
 )
 
 // Actions are functions that respond to a given IRC message with a different IRC message.
-type Action func(*struc.IRCMessage) *struc.IRCMessage
+type Action func(*struc.IRCMessage, []string, map[string]string) *struc.IRCMessage
 
 // NoAction defines the basic action to take in receipt of a message: none.
-func NoAction(msg *struc.IRCMessage) *struc.IRCMessage {
+func NoAction(msg *struc.IRCMessage, args []string, kwargs map[string]string) *struc.IRCMessage {
 	return nil
 }
