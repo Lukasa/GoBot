57efe170834bd99d2e167ba269281d04deebe0d1
Cory Benfield <lukasaoz@gmail.com>
2013-07-21 12:07:47 +0100

    Define the main messaging loops.

---
+++ irc/messageloop.go
@@ -0,0 +1,57 @@
+package irc
+
+import (
+	"github.com/Lukasa/GoBot/struc"
+)
+
+// DispatchMessages loops infinitely, accepting parsed versions of received IRC messages and dispatching them to botscripts.
+// These botscripts return any messages they want to send on the 'out' channel.
+// This can be viewed as the 'main' loop in GoBot.
+func DispatchMessages(in, out chan *struc.IRCMessage, scripts []Botscript) {
+	// Before we begin looping, set the botscripts running. Each one has its own dedicated input channel.
+	chans := make([]chan *struc.IRCMessage, 0)
+
+	for _, script := range scripts {
+		newChan := make(chan *struc.IRCMessage)
+		go script(newChan, out)
+		chans = append(chans, newChan)
+	}
+
+	// Pull messages off the input channel and dispatch them to each botscript.
+	for {
+		msg, ok := <-in
+		if !ok {
+			break
+		}
+
+		for _, channel := range chans {
+			channel <- msg
+		}
+	}
+}
+
+// ParsingLoop provides a tight loop that pops values off the input channel and dispatches goroutines to parse them. This loop
+// is very small to attempt to avoid bottlenecking.
+func ParsingLoop(in chan []byte, out chan *struc.IRCMessage) {
+	for {
+		unparsed, ok := <-in
+		if !ok {
+			break
+		}
+
+		go ParseIRCMessage(unparsed, out)
+	}
+}
+
+// UnParsingLoop provides a tight loop that pops values off the input channel and dispatches goroutines to unparse them. This
+// loop is very small to avoid bottlenecking, as it will get quite a lot of traffic.
+func UnParsingLoop(in chan *struc.IRCMessage, out chan []byte) {
+	for {
+		parsed, ok := <-in
+		if !ok {
+			break
+		}
+
+		go UnparseIRCMessage(parsed, out)
+	}
+}
